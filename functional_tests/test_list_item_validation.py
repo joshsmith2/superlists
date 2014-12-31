a8ac5d308cc7c0f772287f34c3503b4656919ec4
Josh Smith <joshsmith2@gmail.com>
2014-12-31 17:14:08 +0000

    CH10: First cut list item empty validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,16 +8,27 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes home, hits enter on an empty box
         self.browser.get(self.server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Gets error message saying no blank items
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Tries again with text, which works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
-        # Peversely, submits second blank
+        # Perversely, submits second blank
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Same warning on the list page
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Can correct the error by adding text
-        self.fail('Write the test')
+        self.browser.find_element_by_id('id_new_item').send_keys("Make tea\n")
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
+
+
